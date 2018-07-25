94a6068032cf0aeeb3559114ed0c4a4bbbc0cc10
jcsansom <jayceesansom@gmail.com>
2018-07-25 19:23:58 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,14 +6,14 @@ def add_a_key_value_pair
   create_olympics_hash[:Atlanta]="1996"
 end
 
-def summer_olympics
-create_olympics_hash.each do |summer_olympics, date|
-  puts "The #{summer_olympics} summer olympics took place in #{date}."
-end
-end
+# def summer_olympics
+# create_olympics_hash.each do |summer_olympics, date|
+#   puts "The #{summer_olympics} summer olympics took place in #{date}."
+# end
+# end
 
-def upcased_cities
-create_olympics_hash.each_key {|summer_olympics| puts summer_olympics.upcase}
-end
+# def upcased_cities
+# create_olympics_hash.each_key {|summer_olympics| puts summer_olympics.upcase}
+# end
 
-puts upcased_cities
+# puts upcased_cities
